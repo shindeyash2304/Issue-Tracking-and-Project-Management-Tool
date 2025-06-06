b2670f5219305552a00d4c832ba736f2003e19be
Dhairya Joshi <77039476+TheRAWagent@users.noreply.github.com>
2025-06-06 15:07:27 +0530

    added cors allowance for www domain

--- api/jira_clone_api/src/main/java/com/jira_clone_api/jira_clone_api/config/CorsConfig.java
+++ api/jira_clone_api/src/main/java/com/jira_clone_api/jira_clone_api/config/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-          .allowedOrigins(frontendUrl)
-          .allowedMethods("*")
-          .allowCredentials(true);
+                .allowedOrigins(frontendUrl, frontendUrl.replace("://", "://www."))
+                .allowedMethods("*")
+                .allowCredentials(true);
     }
 }
